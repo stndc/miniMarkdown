dbec329c00f0fbc21cfb551ffffd7c942a35b1ae
stn <stn_dc@protonmail.com>
2022-05-03 14:38:16 -0300

    new version 2.x

--- miniMarkdown.php
+++ miniMarkdown.php
@@ -3,41 +3,6 @@
 /*
 STN
 web: stn.tumblr.com
-<<<<<<< HEAD
-miniMarkdown v1.0.0
-*/
-
-function minimarkdown($text) {
-	//H3 Tag
-	$pattern = '/(\*\*(.*?)\*\*)/';
-	$replace = '<h3>${2}</h3>';
-	$text = preg_replace($pattern, $replace, $text);
-
-	//Italic
-	$pattern = '/(_(.*?)_)/';
-	$replace = '<em>${2}</em>';
-	$text = preg_replace($pattern, $replace, $text);
-
-	//Bold
-	$pattern = '/(\*(.*?)\*)/';
-	$replace = '<strong>${2}</strong>';
-	$text = preg_replace($pattern, $replace, $text);
-	
-	//Code
-	$pattern = '/(\`(.*?)\`)/';
-	$replace = '<span style="background-color: #EEEEEE; font-family: monospace; font-size: 1rem; color: #2D333B; padding: .4rem .5rem;">${2}</span>';
-	$text = preg_replace($pattern, $replace, $text);
-
-	//Positive
-	$pattern = '/(\+(.*?)\+)/';
-	$replace = '<span style="background-color: #DDFFDD; font-size: 1rem; color: #2D333B;">${2}</span>';
-	$text = preg_replace($pattern, $replace, $text);
-
-	//Negative
-	$pattern = '/(\~(.*?)\~)/';
-	$replace = '<span style="background-color: #FFDDDD; font-size: 1rem; color: #2D333B;">${2}</span>';
-	$text = preg_replace($pattern, $replace, $text);
-=======
 miniMarkdown v2.x
 */
 
@@ -75,13 +40,9 @@ function minimarkdown($text) {
 
 	//Email
 	$text = preg_replace('/\[email\](.+)\[\/email\]/', '<a href="mailto:$1">$1</a>', $text);
->>>>>>> b66fddc (new version)
-
+	
 	return $text;
 }
-
-<<<<<<< HEAD
-=======
 
 // $text = '# H1
 // ## H2
@@ -96,5 +57,4 @@ function minimarkdown($text) {
 // [email](mailto:mail@mail.com)';
 // echo minimarkdown($text);
 
->>>>>>> b66fddc (new version)
 ?>
